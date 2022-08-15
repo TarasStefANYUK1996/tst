63e9d97d28a399c6e99dbbfc01e58a6d7de2bb0f
TarasStefANYUK1996 <uu254256@gmail.com>
2022-08-15 15:58:58 +0300

    news

--- src/App.js
+++ src/App.js
@@ -5,33 +5,14 @@ import TaskList from './components/TaskList/TaskList';
 import './App.css';
 import Notice from "./components/notice/notice";
 import Cookies from 'universal-cookie';
-// const hosting='http://localhost:3003';
+
 const hosting='https://raw.githubusercontent.com/TarasStefANYUK1996/new/main/src/server/db.json';
 function App() {
   let ret;
   const cookies = new Cookies();
   const [style, setStyle] = useState(null);
   const [notText, setNotText] = useState("");
-  const upData = async (method, id = '', status, newData = null) => {
-    let datal = await getData(hosting+'/posts/' + id);
-    if (method === 'POST') {
-      datal = newData;
-    } else
-      if (method === "PUT") {
-        datal.state = status;
-      }
-    try {
-      await fetch(hosting+'/posts/' + id, {
-        method: method, // или 'PUT'
-        body: JSON.stringify(datal), // данные могут быть 'строкой' или {объектом}!
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    } catch (error) {
-      console.error('Ошибка:', error);
-    }
-  }
+
   const [data, setData] = useState([]);
   const [optionValue, setOptionValue] = useState("active");
 
@@ -91,20 +72,9 @@ function App() {
         setNotText({type:"выполнили задание ",text: data[serch].text});
         data[serch].state = "complete";
         cookies.set('data', data, { path: '/' })
-
       }
-      // 45
-      // setData(data);
-      // setStyle("opaAct");
-      // setTimeout(() => {
-      //   setStyle(false);
-
-      // }, 3000);
-    }
+        }
     
-
-
-
     setData(data);
     setStyle("opaAct");
     setTimeout(() => {
@@ -112,42 +82,30 @@ function App() {
 
     }, 3000);
   }
-  // const save = async (e, id, filter) => {
-  //   const serch = data.findIndex((element, index, array) => {
-  //     return element.id === id;
-  //   });
-   
 
-  //   if (e.closest(".save") != null) {
-  //     if (optionValue === "delete" || optionValue === "complete") {
-  //       data[serch].state = "active";
-  //       cookies.set('data', data, { path: '/' })
-  //       setNotText("вернули задание " + data[serch].text + " на доработку");
-  //     } else {
-  //       setNotText("выполнили задание " + data[serch].text);
-  //       data[serch].state = "complete";
-  //       cookies.set('data', data, { path: '/' })
-
-  //     }
-  //     setData(data);
-  //     setStyle("opaAct");
-  //     setTimeout(() => {
-  //       setStyle(false);
-
-  //     }, 3000);
-  //   }
-  // }
   useEffect(() => {
 
     if (cookies.get('data')) {
       setData(cookies.get('data'));
     } else {
-      getData(hosting+"/posts").then((data) => {
-        setData(data);
-        cookies.set('data', data, { path: '/' });
 
-      });
-    }
+     let datas=[
+          {
+            "text": "Действие 10 ",
+            "state": "active",
+            "id": 3
+          },
+          {
+            "text": "Действие первое ",
+            "state": "active",
+            "id": 1
+          }
+        ]
+      console.log(datas)
+      setData(datas);
+      cookies.set('data', datas, { path: '/' });
+
+       }
 
   }, []);
   ret = style != null ? <Notice styles={style} notText={notText} /> : null
